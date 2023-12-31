bfcced5a6b561a51a679230d102b79511a827ead
raflimuhammadd <raflimuhh09@gmail.com>
2023-12-31 22:19:59 +0700

    add components

--- src/components/About/AboutDetail.jsx
+++ src/components/About/AboutDetail.jsx
@@ -5,7 +5,7 @@ const AboutDetail = () => {
                 <div className='row'>
                     <div className='col-md-8 mt-3'>
                         <h1 className='mt-5 text-dark display-6 pb-4'>
-                            <span className='text-primary'>Lorem.</span> Lorem.
+                            <span className='text-primary'>About.</span> Me.
                         </h1>
                         <p className='col-md-8 fs-5 text-dark text-justify'>
                             As a 5th-semester undergraduate student pursuing a degree in Informatics Engineering at UPN Veteran East Java, I have a keen interest in the world of information and computer technology, with a specific emphasis on cloud computing. Through my studies, I have gained a solid grasp of fundamental concepts in these fields and have honed my programming abilities.
